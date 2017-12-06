a1513068c4e75a2081bc27f0daf82df326ef6019
Alexander Solovyov <solovyov.alexander.a@gmail.com>
2017-12-07 02:08:34 +0300

    Added solution to "Tree height" problem

---
+++ Stepic/Algorithms_structs/Basic_structures/Tree_height/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using gen = System.Collections.Generic;
+
+namespace Tree_height
+{
+    class Program
+    {
+        private class Node
+        {
+            public Node()
+            {
+                _children = new gen.List<Node>();
+            }
+
+            public void addChildren(Node node)
+            {
+                _children.Add(node);
+            }
+
+            public uint Height()
+            {
+                if (_height == 0)
+                {
+                    _height = _children.Count == 0 ? 1 : _children.Select(t => t.Height()).Max() + 1;
+                }
+                return _height;
+            }
+
+            private uint _height;
+            private readonly gen.List<Node> _children;
+        }
+
+        static void Main(string[] args)
+        {
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var n = int.Parse(Console.ReadLine());
+            var nodesParents = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var nodes = (new Node[n]).Select(t => new Node()).ToArray();
+            Node root = null;
+            for (var i = 0; i < n; ++i)
+            {
+                if (nodesParents[i] == -1)
+                {
+                    root = nodes[i];
+                }
+                else
+                {
+                    nodes[nodesParents[i]].addChildren(nodes[i]);
+                }
+            }
+
+            Console.WriteLine("{0}", root.Height());
+        }
+    }
+}
