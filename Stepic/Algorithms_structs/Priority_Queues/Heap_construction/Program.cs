ac1f57db32af8a8d0a314fce7ceab0661117a126
Alexander Solovyov <solovyov.alexander.a@gmail.com>
2017-12-08 17:18:18 +0300

    "PriorityQueue" problems solved

---
+++ Stepic/Algorithms_structs/Priority_Queues/Heap_construction/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Pair = System.Tuple<int, int>;
+using List = System.Collections.Generic.List<System.Tuple<int, int>>;
+
+namespace Heap_construction
+{
+    class Program
+    {
+        private static void Swap(ref int lhs, ref int rhs)
+        {
+            var tmp = rhs;
+            rhs = lhs;
+            lhs = tmp;
+        }
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var pairs = new List();
+
+            for (var i = n / 2; i > 0; --i)
+            {
+                var j = i;
+                while (2 * j <= n)
+                {
+                    if (2 * j < n && array[2 * j] < array[2 * j - 1] && array[2 * j] < array[j - 1])
+                    {
+                        Swap(ref array[j - 1], ref array[2 * j]);
+                        pairs.Add(new Pair(j - 1, 2 * j));
+                        j = 2 * j + 1;
+                    }
+                    else if (array[j - 1] > array[2 * j - 1])
+                    {
+                        Swap(ref array[j - 1], ref array[2 * j - 1]);
+                        pairs.Add(new Pair(j - 1, 2 * j - 1));
+                        j = 2 * j;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("{0}", pairs.Count);
+            foreach (var pair in pairs)
+            {
+                Console.WriteLine("{0} {1}", pair.Item1, pair.Item2);
+            }
+        }
+    }
+}
---
+++ Stepic/Algorithms_structs/Priority_Queues/Parallel_processing/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parallel_processing
+{
+    class Program
+    {
+        private class Pair : IComparable<Pair>
+        {
+            public Pair(uint index, ulong time = 0)
+            {
+                Index = index;
+                Time = time;
+            }
+
+            public int CompareTo(Pair other)
+            {
+                return Time == other.Time ? Index.CompareTo(other.Index) : Time.CompareTo(other.Time);
+            }
+
+            public uint Index { get; }
+            public ulong Time { get; }
+        }
+        
+        private class Heap<T> where T : IComparable<T>
+        {
+            public Heap()
+            {
+                _elements = new List<T>();
+            }
+
+            private void Swap(int i, int j)
+            {
+                T tmp = _elements[i];
+                _elements[i] = _elements[j];
+                _elements[j] = tmp;
+            }
+
+            private void SiftDown(int index)
+            {
+                ++index;
+                while (2 * index <= Count())
+                {
+                    if (2 * index < Count() && 
+                        _elements[2 * index].CompareTo(_elements[2 * index - 1]) < 0 && 
+                        _elements[2 * index].CompareTo(_elements[index - 1]) < 0)
+                    {
+                        Swap(index - 1, 2 * index);
+                        index = 2 * index + 1;
+                    }
+                    else if (_elements[index - 1].CompareTo(_elements[2 * index - 1]) > 0)
+                    {
+                        Swap(index - 1, 2 * index - 1);
+                        index *= 2;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            private void SiftUp(int index)
+            {
+                ++index;
+                while (index != 1 && _elements[index - 1].CompareTo(_elements[index / 2 - 1]) < 0)
+                {
+                    Swap(index - 1, index / 2 - 1);
+                    index /= 2;
+                }
+            }
+
+            public void Push(T value)
+            {
+                _elements.Add(value);
+                SiftUp(Count() - 1);
+            }
+
+            public T Pop()
+            {
+                if (Empty())
+                {
+                    return default(T);
+                }
+
+                var value = _elements.First();
+                Swap(0, Count() - 1);
+                _elements.RemoveAt(Count() - 1);
+                SiftDown(0);
+                return value;
+            }
+
+            public T Peek() => Empty() ? default(T) : _elements.First();
+
+            public int Count() => _elements.Count;
+            public bool Empty() => _elements.Count == 0;
+
+            private readonly List<T> _elements;
+        }
+
+        static void Main(string[] args)
+        {
+            var procNumber = Console.ReadLine().Split(' ').Select(int.Parse).First();
+            var processingTime = Console.ReadLine().Split(' ').Select(ulong.Parse).ToArray();
+            var heap = new Heap<Pair>();
+
+            for (uint i = 0; i < procNumber; ++i)
+            {
+                heap.Push(new Pair(i));
+            }
+
+            foreach (var time in processingTime)
+            {
+                var p = heap.Pop();
+                Console.WriteLine("{0} {1}", p.Index, p.Time);
+                heap.Push(new Pair(p.Index, p.Time + time));
+            }
+        }
+    }
+}
